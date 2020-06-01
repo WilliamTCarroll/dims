190226a20f8610490d570a5abfcb2ce7ba078297
William <antagonisticapple@gmail.com>
2020-05-31 21:37:20 -0500

    Use macro for SI units

--- dims/src/si/volume.rs
+++
@@ -1,7 +0,0 @@
-use crate::systems::Volume;
-use dims_core::unit_creation::*;
-pub const LITRE: UnitSimple<Volume> = UnitSimple::<Volume> {
-    system: PhantomData,
-    ratio: 1.0,
-    offset: 0.0,
-};
