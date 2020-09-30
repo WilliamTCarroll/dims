cb652038bdc7368a47d38cfa4e8d457e70f1990d
William <antagonisticapple@gmail.com>
2020-09-30 15:46:22 -0500

    Add a test crate for nostd

---
+++ test_nostd/src/lib.rs
@@ -0,0 +1,3 @@
+#![no_std]
+mod test_dims;
+mod test_new;
---
+++ test_nostd/src/test_new.rs
@@ -0,0 +1,36 @@
+extern crate dims_core;
+
+use dims_core::unit_creation::*;
+#[derive(Copy, Clone)]
+struct Length;
+impl MeasureSystem for Length {}
+
+/// Size (in bytes) for each floating point number
+const SIZE: usize = 8;
+
+static INCH: UnitSimple<Length> = UnitSimple::<Length> {
+    system: PhantomData,
+    offset: 0.0,
+    ratio: 0.0254,
+    // The following fields should fail (even though intellisense isn't quite accurate)
+    // abbr: "in",
+    // singular: "inch",
+    // plural: "inches",
+};
+
+#[test]
+fn test_size() {
+    use core::mem::{size_of, size_of_val};
+    let raw = 25.4;
+    let wrap = INCH.from(raw);
+
+    // These should all be the same:
+    // The number of bytes equal to the stored value
+    assert_eq!(SIZE, size_of::<Measure<Length>>());
+    assert_eq!(SIZE, size_of_val(&wrap));
+    assert_eq!(SIZE, size_of_val(&raw));
+    // Two floats and no strings
+    assert_eq!(SIZE + SIZE, size_of_val(&INCH));
+    // These are just marker structs; no memory is allocated
+    assert_eq!(0, size_of::<Length>());
+}
