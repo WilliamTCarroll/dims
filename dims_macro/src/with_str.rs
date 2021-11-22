ceb91e19bcd783d2032966291bae78d7d414d1d5
William <William>
2021-11-21 22:44:44 -0600

    Modify System creation macro and move to `dims_macro`
    The struct is now generated directly

--- dims_macro/src/with_str.rs
+++ dims_macro/src/with_str.rs
@@ -12,7 +12,7 @@ macro_rules! one_unit {
         $abbr1:literal,
         $abbr2:literal) => {
         $crate::paste::item! {
-            pub static [<$prefix:upper $unit_pre:upper $base:upper>]: dims_core::unit_creation::UnitFormat<$system> =
+            pub const [<$prefix:upper $unit_pre:upper $base:upper>]: dims_core::unit_creation::UnitFormat<$system> =
             dims_core::unit_creation::UnitFormat::<$system> {
                 system: dims_core::unit_creation::PhantomData,
                 ratio: repeat_item!($ratio, $repeat),
